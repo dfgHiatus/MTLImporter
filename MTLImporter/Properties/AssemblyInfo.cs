7232fda22010534a3706040d210ffdc4d3836a67
Delta <delta@deltawolf.net>
2025-04-03 14:26:41 -0700

    Update Assembly Info with Version constant

--- MTLImporter/Properties/AssemblyInfo.cs
+++ MTLImporter/Properties/AssemblyInfo.cs
@@ -1,35 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MTLImporter")]
-[assembly: AssemblyDescription("Imports Wavefront .mtl directly into Resonite as material orbs.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hiatus Labs")]
 [assembly: AssemblyProduct("MTLImporter")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0322B2EF-7452-479D-BAE2-FCAB75033337")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyDescription("Imports Wavefront .mtl directly into Resonite as material orbs.")]
+[assembly: AssemblyCompany("Hiatus Labs")]
+[assembly: AssemblyCopyright("Copyright © 2025 Hiatus Labs")]
+[assembly: AssemblyVersion(MTLImporter.MTLImporter.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(MTLImporter.MTLImporter.VERSION_CONSTANT)]
